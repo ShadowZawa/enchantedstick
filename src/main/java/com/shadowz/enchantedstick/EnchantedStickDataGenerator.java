3ee6914841d40b0ecc84fffdcee9c2254cd9e512
ShadowZ <97100803+ShadowZawa@users.noreply.github.com>
2026-03-18 23:59:06 +0800

    Initial commit

---
+++ src/main/java/com/shadowz/enchantedstick/EnchantedStickDataGenerator.java
@@ -0,0 +1,11 @@
+package com.shadowz.enchantedstick;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnchantedStickDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
